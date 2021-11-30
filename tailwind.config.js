b99e4189f7b2c4582019ff0cd5ab00d1583ea058
huan5678 <huan5678@gmail.com>
2021-12-01 01:18:57 +0800

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,78 @@
+module.exports = {
+  mode: 'jit',
+  purge: {
+    content: [
+      './index.html',
+      './src/**/*.{vue,js,ts}',
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#6A33FF',
+          dark: '#301E5F',
+          md:"#9D7FEA",
+          light: "#DACBFF",
+        },
+        gray: {
+          DEFAULT: '#BFBFBF',
+          dark: "#797979",
+          light: "#F8F8F8",
+        },
+        secondary: {
+          DEFAULT: '#818A91',
+          light: "#CED4DA",
+        },
+        blue: "#0067CE",
+        red: "#C44021",
+      },
+                fontFamily: {
+            sans: [
+              "Noto Sans TC",
+              "ui-sans-serif",
+              "system-ui",
+              "-apple-system",
+              "BlinkMacSystemFont",
+              "Segoe UI",
+              "Roboto",
+              "Helvetica Neue",
+              "Arial",
+              "Noto Sans",
+              "sans-serif",
+              "Apple Color Emoji",
+              "Segoe UI Emoji",
+              "Segoe UI Symbol",
+              "Noto Color Emoji",
+            ],
+          },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    ({ addComponents }) => {
+              addComponents({
+                ".container": {
+                  maxWidth: "100%",
+                  "@screen sm": {
+                    maxWidth: "540px",
+                  },
+                  "@screen md": {
+                    maxWidth: "768px",
+                  },
+                  "@screen lg": {
+                    maxWidth: "960px",
+                  },
+                  "@screen xl": {
+                    maxWidth: "1110px",
+                  },
+                },
+              });
+            },
+  ],
+};

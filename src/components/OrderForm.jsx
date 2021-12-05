006dc1da38babb3fdf45100a3c051e167cc2f160
huan5678 <huan5678@gmail.com>
2021-12-06 01:18:08 +0800

    react-hook-form

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,73 +1,114 @@
-import {useForm} from 'react-hook-form';
+import {useForm , Controller} from 'react-hook-form';
+import {useEffect} from 'preact/hooks';
 
 export function OrderForm({ orderData, setOrderData }) {
   const {
     register,
     handleSubmit,
+    control,
     watch,
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => console.log(data);
-  console.log(watch("example"));
+  const watchUserName = watch("userName");
+  const watchUserEmail = watch("userEmail");
+  const watchUserTel = watch("userTel");
+  const watchUserAddress = watch("userAddress");
+  const watchAllFields = watch();
+  useEffect(() => {
+    const formData = watch((value, { name, type }) => console.log(value, name, type));
+
+  }, [watch]);
   return (
     <section class="container pt-14 pb-20">
-      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {/* register your input into the hook by invoking the "register" function */}
-        <input defaultValue="test" {...register("example")} />
-
-        {/* include validation with required or other standard HTML validation rules */}
-        <input {...register("exampleRequired", { required: true })} />
-        {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <span>This field is required</span>}
-
-        <input type="submit" />
-      </form>
       <h2 class="text-center text-h2 mb-8">填寫預訂資料</h2>
-      <form action="" class="space-y-5 md:w-1/2 mx-auto">
+      <form
+        action=""
+        class="space-y-5 md:w-1/2 mx-auto"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Controller
+          name="test"
+          control={control}
+          rules={{ required: true }}
+          render={({
+            field: { onChange, onBlur, value, name, ref },
+            fieldState: { invalid, isTouched, isDirty, error },
+            formState,
+          }) => (
+            <input
+              onBlur={onBlur} // notify when input is touched
+              onChange={onChange} // send value to hook form
+              inputRef={ref}
+              class="form-control"
+            />
+          )}
+        />
         <div>
           <label class="block mb-2" for="userName">
             姓名
           </label>
-          <input
-            id="userName"
-            type="text"
-            class="text-secondary rounded border border-secondary-light focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent py-2 px-3 w-full"
-            placeholder="請輸入姓名"
-          />
+          <div class="flex gap-2 items-center">
+            <input
+              id="userName"
+              name="userName"
+              type="text"
+              {...register("userName", { required: true })}
+              class="form-control"
+              placeholder="請輸入姓名"
+            />
+            {errors.userName && <span class="text-red flex-none">必填!</span>}
+          </div>
         </div>
         <div>
           <label class="block mb-2" for="userTel">
             電話
           </label>
-          <input
-            id="userTel"
-            type="tel"
-            class="text-secondary rounded border border-secondary-light focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent py-2 px-3 w-full"
-            placeholder="請輸入電話"
-          />
+          <div class="flex gap-2 items-center">
+            <input
+              id="userTel"
+              name="userTel"
+              type="tel"
+              {...register("userTel", { required: true })}
+              class="form-control"
+              placeholder="請輸入電話"
+            />
+            {errors.userTel && <span class="text-red flex-none">必填!</span>}
+          </div>
         </div>
         <div>
           <label class="block mb-2" for="userEmail">
             Email
           </label>
-          <input
-            id="userEmail"
-            type="email"
-            class="text-secondary rounded border border-secondary-light focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent py-2 px-3 w-full"
-            placeholder="請輸入Email"
-          />
+          <div class="flex gap-2 items-center">
+            <input
+              id="userEmail"
+              name="userEmail"
+              type="email"
+              {...register("userEmail", { required: true })}
+              class="form-control"
+              placeholder="請輸入Email"
+            />
+            {errors.userEmail && <span class="text-red flex-none">必填!</span>}
+          </div>
         </div>
         <div>
           <label class="block mb-2" for="userAddress">
             寄送地址
           </label>
-          <input
-            id="userAddress"
-            type="text"
-            class="text-secondary rounded border border-secondary-light focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent py-2 px-3 w-full"
-            placeholder="請輸入寄送地址"
-          />
+          <div class="flex gap-2 items-center">
+            <input
+              id="userAddress"
+              name="userAddress"
+              type="text"
+              {...register("userAddress", { required: true })}
+              class="form-control"
+              placeholder="請輸入寄送地址"
+            />
+            {errors.userAddress && (
+              <span class="text-red flex-none">必填!</span>
+            )}
+          </div>
         </div>
         <div>
           <label class="block mb-2" for="payment">
@@ -75,7 +116,8 @@ export function OrderForm({ orderData, setOrderData }) {
           </label>
           <select
             id="payment"
-            class="text-secondary rounded border border-secondary-light focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent py-2 px-3 w-full"
+            name="payment"
+            class="form-control"
           >
             <option value="ATM">ATM</option>
             <option value="creditCard">信用卡</option>

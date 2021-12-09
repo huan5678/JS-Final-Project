904a70ad9e963e54e72acf44e575331cdb02188e
huan5678 <huan5678@gmail.com>
2021-12-10 00:51:36 +0800

    dashboard

--- src/components/OderList.jsx
+++ src/components/OderList.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'preact/hooks';
 import FetchData from "./FetchData";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+import { Pie } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -10,19 +10,9 @@ export const data = {
   datasets: [
     {
       label: "# of Votes",
-      data: [ 3, 5, 2, 3],
-      backgroundColor: [
-        "#DACBFF",
-        "#9D7FEA",
-        "#5434A7",
-        "#301E5F",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(153, 102, 255, 1)",
-      ],
+      data: [3, 5, 2, 3],
+      backgroundColor: ["#DACBFFCC", "#9D7FEACC", "#5434A7CC", "#301E5FCC"],
+      borderColor: ["#DACBFF", "#9D7FEA", "#5434A7", "#301E5F"],
       borderWidth: 1,
     },
   ],
@@ -42,7 +32,7 @@ const OderList = ({ ordersList, setOrdersList }) => {
 
   return (
     <section class="container">
-      <Doughnut data={data} />
+      <Pie data={data} width={50} height={50}  />
       <div class="flex flex-wrap space-y-3">
         <button
           class="ml-auto rounded py-2 px-5 border border-black transition duration-300 ease-in-out hover:bg-black hover:border-transparent hover:text-white"

48a720a6dd33e743e5e4c6d31089c291ed1c10a9
huan5678 <huan5678@gmail.com>
2021-12-15 18:13:03 +0800

    update

--- src/pages/NotFind.jsx
+++ src/pages/NotFind.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Img404 from '../images/Img404.svg';
 
 const NotFind = () => {
   return (
     <div className="container">
+      <Img404 />
       <h1 className="text-h1 text-center">404</h1>
       <h2 className="text-h2 text-center">Page not found</h2>
       <p className="text-lg text-center">勝敗乃兵家常事，<br />大俠請重新來過。</p>
